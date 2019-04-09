8f5bcef13d6b82ec26bea13e3d31e93216d3bcce
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-04-09 00:16:41 +0000

    completed avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,31 @@
 var request = require('request');
+var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization' : 'token ' + secrets.GITHUB_TOKEN
+    }
+  };
 
+  request(options, function(err, res, body) {
+    var data = JSON.parse(body)
+
+    cb(err, data);
+
+    data.forEach(function(keys) {
+      console.log(keys.avatar_url);
+    });
+
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
+
 });
